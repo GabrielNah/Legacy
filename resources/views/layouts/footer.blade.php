fa400c1424f36358daf8a9a9bc79e427fbe2baac
Aramayis Gharibyan <aramayis.gharibyan@yandex.ru>
2022-09-07 22:02:58 +0400

    Footer fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,62 +1,58 @@
 <div class="section pp-scrollable slide slide2 slide-dark">
     <div class="slide-container">
-        <div class="container">
-            <div class="row slide-content">
-                <div class="col-sm-6">
+        <div class="container container-fluid">
+            <div class="row slide-content justify-content-between">
+                <div class="col-lg-4  col-sm-4">
                     <h2 class="slide-title font-libre h2"><span class="text-primary">МОСКВА</span></h2>
                     <div class="slide-body">
                         <div class="slide-contact-address">ЛЕНИНГРАДСКИЙ ПР., Д.80, КОРПУС 21, ЭТАЖ 2,
                             ПОМЕЩЕНИЕ 1
                         </div>
-                        <div class="slide-contact-mail"><a href="mailto:info@nasledie-gk.ru"
-                                                           class="underline">info@nasledie-gk.ru</a></div>
-                        <div class="slide-contact-phone"><a href="tel:+74995505592" class="phone-link">8 499
-                                550-5592</a></div>
+                        <div class="slide-contact-mail">
+                            <a href="mailto:info@nasledie-gk.ru" class="underline">info@nasledie-gk.ru</a>
+                        </div>
+                        <div class="slide-contact-phone mb-sm-3">
+                            <a href="tel:+74995505592" class="phone-link">8 499550-5592</a>
+                        </div>
+
+                        <a class="btn btn-primary btn-rounded mr-4" href="contact.html">Связаться
+                            <i class="icon ion-ios-arrow-right"></i>
+                        </a>
                     </div>
                 </div>
-                <div class=" col-lg-8">
-                    <form action="#">
-                        <div class="row">
+                <div class="col-lg-7 col-sm-8">
+                    <form class="contact-form" action="#">
+                        <div class="row ">
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" placeholder="Имя"
-                                           class="form-control mt-3 form-rounded">
+                                    <input type="text" placeholder="Имя" class="form-control form-rounded">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" placeholder="Компания"
-                                           class="form-control mt-3 form-rounded">
+                                    <input type="text" placeholder="Компания" class="form-control form-rounded">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="email" placeholder="мейл"
-                                           class="form-control mt-3 form-rounded">
+                                    <input type="email" placeholder="E-mail" class="form-control form-rounded">
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="number" placeholder="тел."
-                                           class="form-control mt-3 form-rounded">
+                                    <input type="number" placeholder="Телефон" class="form-control form-rounded">
                                 </div>
                             </div>
                             <div class="col-12">
-                                            <textarea placeholder="сообщение"
-                                                      class="form-control mt-3 form-rounded"></textarea>
+                                <textarea placeholder="Сообщение" class="form-control mt-3 form-rounded"></textarea>
                                 <button class="btn btn-primary btn-rounded form-control mt-4 form-rounded">
                                     отправить
                                 </button>
                             </div>
-
-
                         </div>
                     </form>
-
                 </div>
             </div>
-            <a class="btn btn-primary btn-rounded mr-4" href="contact.html">Связаться <i
-                    class="icon ion-ios-arrow-right"></i></a>
         </div>
     </div>
 </div>
